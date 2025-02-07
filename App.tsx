5c1c46d9d5fbdcc721fdbb417408e4906841d824
Son-2003 <113695548+Son-2003@users.noreply.github.com>
2025-02-07 21:07:03 +0700

    update file

--- App.tsx
+++ App.tsx
@@ -2,18 +2,9 @@ import React, { useEffect, useState } from "react";
 import AppNavigator from "./src/navigation/AppNavigator";
 import "./locales/i18n";
 import { NavigationContainer } from "@react-navigation/native";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
 import SplashScreen from "./src/screens/SplashScreen";
 import { SafeAreaView } from "react-native-safe-area-context";
-import SignUpScreen from "./src/screens/AuthenScreen/SignUpScreen";
-import ForgotPassScreen from "./src/screens/AuthenScreen/ForgotPassScreen";
-import ForgotPassSuccessScreen from "./src/screens/AuthenScreen/ForgotPassSuccessScreen";
-import ResetPassword from "./src/screens/AuthenScreen/ResetPasswordScreen";
-import SignUpSuccessScreen from "./src/screens/AuthenScreen/SignUpSuccessScreen";
-import VerifyPhoneScreen from "./src/screens/AuthenScreen/VerifyPhoneScreen";
-import OTPScreen from "./src/screens/AuthenScreen/OTPScreen";
-import SignInScreen from "./src/screens/AuthenScreen/SignInScreen";
-import ResetPasswordScreen from "./src/screens/AuthenScreen/ResetPasswordScreen";
 
 const App: React.FC = () => {
   const [isLoading, setIsLoading] = useState<boolean>(true);
